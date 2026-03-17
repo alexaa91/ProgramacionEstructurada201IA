b2bca018629af389b309fc50085db4e4554fb492
Alexa <alexaalarcon71@gmail.com>
2026-03-17 07:30:09 -0600

    Ejercicios aritmeticos

---
+++ UX.COM.CURSO.PROGRAMACION/Unidad1/ejercicios_aritmetica/ejercicios_aritmeticos.py
@@ -0,0 +1,25 @@
+def mostrar_operadores():
+    # SUMA
+    suma = 5 + 3
+    print("Suma 5 + 3 =", suma)
+    # RESTA
+    resta = 5 - 3
+    print("Resta 5 + 3 =", resta)
+    # MULTIPLICACION
+    multiplicacion = 6 * 2
+    print("multiplicacion 6 * 2 =", multiplicacion)
+    # DIVISION ENTEROS
+    division = 10 // 3
+    print("Division 10 // 3 =", division)
+    # MODULO 
+    modulo = 10 % 3
+    print("Modulo 10 % 3 =", modulo)
+    # POTENCIACION
+    potenciacion = 2 ** 3
+    print("Potencia 2 ** 3 =", potenciacion)
+
+def main():
+    mostrar_operadores()
+
+if __name__=="__main__":
+    main()
