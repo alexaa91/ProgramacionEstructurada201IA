ddfd6b550f26b0179eb4c0f0d49407b9a731a0aa
Alexa <alexaalarcon71@gmail.com>
2026-03-27 08:29:40 -0600

    Ejercicio mermaid

---
+++ UX.COM.CURSO.PROGRAMACION/Unidad2/mermaid/ejercicio_01.py
@@ -0,0 +1,15 @@
+def es_divisible():
+    suma= 0
+    i = 1
+    while i <=100:
+        if i % 3 ==0 and i%2!=0:
+            suma += i
+        i += 1
+    return suma
+
+def main():
+    suma = es_divisible()
+    print(suma)
+
+if __name__ == "__main__":
+    main()
