3ed8380ba9dfa4ae246e60290736364fd3727830
Alexa <alexaalarcon71@gmail.com>
2026-04-07 07:53:27 -0600

    Ejercicio Mermaid 6

---
+++ UX.COM.CURSO.PROGRAMACION/Unidad2/mermaid/ejercicio_06.py
@@ -0,0 +1,14 @@
+def fondos():
+    saldo = 0
+    meta = 1000
+    while saldo < meta:
+        deposito = int(input("Ingrese el deposito: "))
+        saldo += deposito
+    return saldo
+
+def main():
+    resultado = fondos()
+    print("Meta superada $", resultado)
+
+if __name__ == "__main__":
+    main()
