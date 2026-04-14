1879c7a8184ef3fac305046e80ac0ab3f32cd5c5
Alexa <alexaalarcon71@gmail.com>
2026-04-14 08:10:37 -0600

    Analizador Predictivo

--- UX.COM.CURSO.PROGRAMACION/Unidad3/ejercicios_IA/practica_02/AnalizadorPredictivo.py
+++ UX.COM.CURSO.PROGRAMACION/Unidad3/ejercicios_IA/practica_02/AnalizadorPredictivo.py
@@ -47,24 +47,40 @@ def generar_reporte(total_datos, validos, estadisticas):
     """
     FUNCIÓN 4: Imprime un resumen formateado de los resultados.
     """
-    # IMPLEMENTAR AQUÍ
+    v_max, v_min, v_prom = estadisticas
+    descartados =  total_datos - validos
+
+    print("*" * 30)
+    print("REPORTE DE ANÁLISIS PRODUCTIVO")
+    print("*" * 30)
+    print(f"Total de lecturas procesadas: {total_datos}")
+    print(f"Lecturas validas: {validos}")
+    print(f"Lecturas descartadas: {descartados}")
+    print(f"Valor máximo: {v_max:.2f}")
+    print(f"Valor mínimo: {v_min:.2f}")
+    print(f"Valor promedio: {v_prom:.2f}")
+    print("*" * 30)
 
 # --- LÓGICA PRINCIPAL (NO MODIFICAR ESTA PARTE) ---
+import os
 def ejecutar_pipeline():
     datos_finales = []
     cuenta_total = 0
 
-    with open("lecturas_sensores.txt", "r") as f:
+    ruta_script = os.path.dirname(os.path.abspath(__file__))
+    ruta_archivo = os.path.join(ruta_script, "lecturas_sensores.txt")
+
+    with open(ruta_archivo, "r") as f:
         for linea in f:
             cuenta_total += 1
             valor = limpiar_dato(linea.strip())
-        if valor is not None:
+            if valor is not None:
             # Normalizar para la IA (0-1)
-            datos_finales.append(valor / 100)
+                datos_finales.append(valor / 100)
 
-        if datos_finales:
-            stats = obtener_estadisticas(datos_finales)
-            generar_reporte(cuenta_total, len(datos_finales), stats)
+    if datos_finales:
+        stats = obtener_estadisticas(datos_finales)
+        generar_reporte(cuenta_total, len(datos_finales), stats)
 
 if __name__ == "__main__":
     ejecutar_pipeline()
