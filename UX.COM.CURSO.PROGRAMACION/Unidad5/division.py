eca36101cfca4d6927caac92a23ab72e5e1f575c
Alexa <alexaalarcon71@gmail.com>
2026-05-26 08:22:36 -0600

    division: realizar división entera y decimal de dos números enteros

---
+++ UX.COM.CURSO.PROGRAMACION/Unidad5/division.py
@@ -0,0 +1,15 @@
+def division():
+    a = int(input("Ingrese el primer número: "))
+    b = int(input("Ingrese el segundo número: "))
+
+    integer_division = a // b
+    print(integer_division)
+
+    division_result = a / b
+    print(division_result)
+
+def main():
+    division()
+
+if __name__ == "__main__":
+    main()
