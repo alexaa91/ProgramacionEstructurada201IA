d616c2e0f08629d7344ac2b115da9e0ee27e3d77
Alexa <alexaalarcon71@gmail.com>
2026-05-26 08:06:52 -0600

    if-else: evaluar números enteros

---
+++ UX.COM.CURSO.PROGRAMACION/Unidad5/if-else.py
@@ -0,0 +1,17 @@
+def integer(n):
+    if n % 2 == 1:
+        print("Weird")
+    else:
+        if 2 <= n <= 5:
+            print("Not Weird")
+        elif 6 <= n <= 20:
+            print("Weird")
+        elif n > 20:
+            print("Not Weird")
+
+def main():
+    n = int(input().strip())
+    integer(n)
+
+if __name__ == "__main__":
+    main()
